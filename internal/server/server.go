2eba3dbbb18f4b9dfae29ac7685f855a45004b37
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-11-04 21:10:29 +0530

    room functionality

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,11 +2,17 @@ package server
 
 import (
 	"flag"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"os"
+	"time"
+	// "github.com/gofiber/template/html"
+	// "github.com/gofiber/websocket/v2"
 )
 
 var (
-	addr = flag.String("addr", os.Getenv("PORT"), "Server address")
+	addr = flag.String("addr", ":"+os.Getenv("PORT"), "")
 	cert = flag.String("cert", "", "Path to SSL certificate")
 	key  = flag.String("key", "", "Path to SSL key")
 )
@@ -18,10 +24,17 @@ func Run() {
 		*addr = ":8080"
 	}
 
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
+	app.Use(logger.New())
+	app.Use(cors.New())
+
 	app.Get("/", handlers.Welcome)
 	app.Get("/room/create", handlers.RoomCreate)
 	app.Get("/room/:uuid", handlers.Room)
-	app.Get("/room/:uuid/websocket")
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebsocket, websocket.Config{
+		HandshakeTimeout: 10 * time.Second,
+	}))
 	app.Get("/room/:uuid/chat", handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
 	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
